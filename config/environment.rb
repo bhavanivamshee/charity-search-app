d416e8fd6ae3d2ead15123515c61d465c6fe44d5
Bhavani <bhavanivamshee@gmail.com>
2021-02-09 22:51:15 -0600

    skeleton for user commands

---
+++ config/environment.rb
@@ -0,0 +1,9 @@
+require 'pry'
+require 'httparty'
+require 'colorize'
+
+
+require_relative('../lib/API')
+require_relative('../lib/charities')
+require_relative('../lib/CLI')
+
--- lib/CLI.rb
+++ lib/CLI.rb
@@ -1,2 +1,49 @@
+#handles all interaction with user
+
 class CLI 
+
+    def call
+        puts ""
+        puts "Welcome to the charity search app!"
+        puts "Please enter your zipcode to find charitable causes in your area"
+        puts "To exit the app, enter 'exit'"
+        puts ""
+        menu
+    end
+
+    def menu
+        input = gets.strip
+
+        if input == "zipcode"
+            puts "zipcode"
+        elsif input == "exit"
+            puts "exit"
+        else
+            puts "Oops! Invalid entry, please try again"
+            menu
+        end
+
+    end
+
+    def zipcode_list
+        puts ""
+        puts "Here is the list of charities in your zipcode"
+        puts "Would you like more information on these charities?"
+        puts "Please enter 'yes', or 'exit'"
+        puts ""
+        second_menu
+    end
+
+    def second_menu
+        second_input = gets.strip.downcase
+        if input == "yes"
+            puts "more information"
+
+        else input == "exit"
+            puts "Thank you for stopping by! Goodbye :)"
+        end
+
+    end
+
+
 end
